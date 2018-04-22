10a6a00f243fcb97a2d436cdf22d623432f21a90
alex <alexliu108@gmail.com>
2018-04-21 21:17:16 -0700

    changed index

--- src/main/java/com/beachacks2018/studentfileanddiscussion/model/Department.java
+++ src/main/java/com/beachacks2018/studentfileanddiscussion/model/Department.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -26,6 +27,9 @@ public class Department implements Serializable {
     private Long id;
     @NotNull
     private String deptName;
+    
+    @ManyToOne
+    private School school;
     
     
     /**
---
+++ src/main/java/com/beachacks2018/studentfileanddiscussion/model/School.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beachacks2018.studentfileanddiscussion.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author alexl
+ */
+@Entity
+@NamedQueries({
+    @NamedQuery(name = School.FIND_ALL_SCHOOLS, query = "SELECT s FROM School s")    
+})
+public class School implements Serializable {
+    
+    public static final String FIND_ALL_SCHOOLS = "School.findAllSchools";
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String schoolName;
+    
+    @OneToMany(mappedBy = "School", cascade = CascadeType.ALL)
+    private Collection<Department> departments;
+    
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getSchoolName(){ 
+        return this.schoolName;
+    }
+    public void setSchoolName(String name){
+        this.schoolName = name;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof School)) {
+            return false;
+        }
+        School other = (School) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return "com.beachacks2018.studentfileanddiscussion.model.School[ id=" + id + " ]";
+    }
+    
+}
