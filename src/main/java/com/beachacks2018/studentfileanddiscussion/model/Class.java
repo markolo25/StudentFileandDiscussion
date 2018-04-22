de5555557f4fc015857053ca8e6f5455deb10473
pekichi0324 <arare19860514@gmail.com>
2018-04-21 21:00:37 -0700

    added message class

--- src/main/java/com/beachacks2018/studentfileanddiscussion/model/Class.java
+++ src/main/java/com/beachacks2018/studentfileanddiscussion/model/Class.java
@@ -6,6 +6,7 @@
 package com.beachacks2018.studentfileanddiscussion.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,7 +23,69 @@ public class Class implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String className;
+    private long classNum;
+  
 
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public long getClassNum() {
+        return classNum;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public void setClassNum(long classNum) {
+        this.classNum = classNum;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" + "id=" + id + ", className=" + className + ", classNum=" + classNum + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.className);
+        hash = 53 * hash + (int) (this.classNum ^ (this.classNum >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Class other = (Class) obj;
+        if (this.classNum != other.classNum) {
+            return false;
+        }
+        if (!Objects.equals(this.className, other.className)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+ 
     public Long getId() {
         return id;
     }
@@ -30,30 +93,7 @@ public class Class implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Class)) {
-            return false;
-        }
-        Class other = (Class) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "com.beachacks2018.studentfileanddiscussion.model.Class[ id=" + id + " ]";
-    }
+    
+   
     
 }
--- src/main/java/com/beachacks2018/studentfileanddiscussion/model/Message.java
+++ src/main/java/com/beachacks2018/studentfileanddiscussion/model/Message.java
@@ -6,7 +6,6 @@
 package com.beachacks2018.studentfileanddiscussion.model;
 
 import java.io.Serializable;
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,13 +16,12 @@ import javax.persistence.Id;
  * @author arare
  */
 @Entity
-public class File implements Serializable {
+public class Message implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String fileName;
 
     public Long getId() {
         return id;
@@ -35,28 +33,19 @@ public class File implements Serializable {
 
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 41 * hash + Objects.hashCode(this.id);
-        hash = 41 * hash + Objects.hashCode(this.fileName);
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
         return hash;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Message)) {
             return false;
         }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final File other = (File) obj;
-        if (!Objects.equals(this.fileName, other.fileName)) {
-            return false;
-        }
-        if (!Objects.equals(this.id, other.id)) {
+        Message other = (Message) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
         return true;
@@ -64,19 +53,7 @@ public class File implements Serializable {
 
     @Override
     public String toString() {
-        return "File{" + "id=" + id + ", fileName=" + fileName + '}';
+        return "com.beachacks2018.studentfileanddiscussion.model.Message[ id=" + id + " ]";
     }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    
-
-    
     
 }
